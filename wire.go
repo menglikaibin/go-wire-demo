bdd195b5d44d30b726dbd4dd4fd57813ef6a06cf
menglikaibin <429148925@qq.com>
2021-04-29 17:17:37 +0800

    绑定值

--- wire.go
+++ wire.go
@@ -4,19 +4,21 @@ package main
 
 import "github.com/google/wire"
 
+var kitty = Monster{Name: "哥斯拉"}
+
 var monsterPlayerSet = wire.NewSet(NewMonster, NewPlayer)
 
 var endingASet = wire.NewSet(monsterPlayerSet, wire.Struct(new(EndingA), "*"))
 var endingBSet = wire.NewSet(monsterPlayerSet, wire.Struct(new(EndingB), "*"))
 
 func InitEndingA(name string) EndingA {
-	wire.Build(endingASet)
+	wire.Build(NewPlayer, wire.Value(kitty), NewEndingA)
 
 	return EndingA{}
 }
 
 func InitEndingB(name string) EndingB {
-	wire.Build(endingBSet)
+	wire.Build(NewPlayer, wire.Value(kitty), NewEndingB)
 
 	return EndingB{}
 }
--- wire_gen.go
+++ wire_gen.go
@@ -13,25 +13,29 @@ import (
 
 func InitEndingA(name string) EndingA {
 	player := NewPlayer(name)
-	monster := NewMonster(name)
-	endingA := EndingA{
-		Player:  player,
-		Monster: monster,
-	}
+	monster := _wireMonsterValue
+	endingA := NewEndingA(player, monster)
 	return endingA
 }
 
+var (
+	_wireMonsterValue = kitty
+)
+
 func InitEndingB(name string) EndingB {
 	player := NewPlayer(name)
-	monster := NewMonster(name)
-	endingB := EndingB{
-		Player:  player,
-		Monster: monster,
-	}
+	monster := _wireMainMonsterValue
+	endingB := NewEndingB(player, monster)
 	return endingB
 }
 
+var (
+	_wireMainMonsterValue = kitty
+)
+
 // wire.go:
+
+var kitty = Monster{Name: "哥斯拉"}
 
 var monsterPlayerSet = wire.NewSet(NewMonster, NewPlayer)
 
