4305f545c02159db9eb919faf227c3d9b04ce1fd
wukaibin <429148925@qq.com>
2021-04-29 16:13:48 +0800

    重复代码抽象

--- wire.go
+++ wire.go
@@ -4,14 +4,16 @@ package main
 
 import "github.com/google/wire"
 
+var monsterPlayerSet = wire.NewSet(NewMonster, NewPlayer)
+
 func InitEndingA(name string) EndingA {
-	wire.Build(NewMonster, NewPlayer, NewEndingA)
+	wire.Build(monsterPlayerSet, NewEndingA)
 
 	return EndingA{}
 }
 
 func InitEndingB(name string) EndingB {
-	wire.Build(NewMonster, NewPlayer, NewEndingB)
+	wire.Build(monsterPlayerSet, NewEndingB)
 
 	return EndingB{}
 }
