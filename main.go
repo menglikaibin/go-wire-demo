891947bb6f2b9864e13fc9a5adfdb2d6d9e1d8d5
DESKTOP-5SDUENI\42914 <wukaibin@linhuiba.com>
2021-04-29 10:56:06 +0800

    go语言wire库demo

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+type Monster struct {
+	Name string
+}
+
+func NewMonster() Monster {
+	return Monster{Name: "kitty"}
+}
+
+type Player struct {
+	Name string
+}
+
+func NewPlayer(name string) Player {
+	return Player{Name: name}
+}
+
+
+type Mission struct {
+	Player  Player
+	Monster Monster
+}
+
+func NewMission(p Player, m Monster) Mission {
+	return Mission{p, m}
+}
+
+func (m Mission) Start() {
+	fmt.Printf("%s defeats %s, world peace!\n", m.Player.Name, m.Monster.Name)
+}
+
+// 使用wire前
+func main() {
+	mission := InitMission("瓜皮")
+
+	mission.Start()
+}
